49baac651b98f9ca48f6d775fb3fa812e3e5b400
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-07-08 06:42:20 +0900

    java_interface_basic_3_1

--- Java/Metod.java
+++ Java/Metod.java
@@ -56,95 +56,105 @@
 // （実行結果）
 // Hello World!
 
-4 メソッドに引数を渡す方法
-・メソッドを呼び出す時に、呼び出し元から使いたい値を渡せます。この時に渡される値のことを「引数」
-・呼び出されたメソッドでは、渡された値を使って処理に使うことが可能
-・引数には、数値や文字列などが指定でき、値や型、引数の数を自由に決められる
-（例：helloメソッドを、名前を渡して呼び出すプログラムに書き換える）
-public class Main {
-  public static void main(String[] args) {
-      hello("Potepan");
-  }
+// 4 メソッドに引数を渡す方法
+// ・メソッドを呼び出す時に、呼び出し元から使いたい値を渡せます。この時に渡される値のことを「引数」
+// ・呼び出されたメソッドでは、渡された値を使って処理に使うことが可能
+// ・引数には、数値や文字列などが指定でき、値や型、引数の数を自由に決められる
+// （例：helloメソッドを、名前を渡して呼び出すプログラムに書き換える）
+// public class Main {
+//   public static void main(String[] args) {
+//       hello("Potepan");
+//   }
   
-  public static void hello(String name) {
-      System.out.println(name + " Hello World!");
-  }
-}
-（実行結果）
-Potepan Hello World!
+//   public static void hello(String name) {
+//       System.out.println(name + " Hello World!");
+//   }
+// }
+// （実行結果）
+// Potepan Hello World!
 
-4_1 メソッドに複数の引数を渡す方法
-・メソッドに複数の引数を渡す場合は、値をカンマ(,)で区切って書く
-	メソッド名(引数1, 引数2, 引数3,・・・);
-（例：calculateメソッドに、2つの値を渡して掛け算をするプログラムを作る）
-public class Main {
-  public static void main(String[] args) {
-      calculate(25, 85);
-  }
+// 4_1 メソッドに複数の引数を渡す方法
+// ・メソッドに複数の引数を渡す場合は、値をカンマ(,)で区切って書く
+// 	メソッド名(引数1, 引数2, 引数3,・・・);
+// （例：calculateメソッドに、2つの値を渡して掛け算をするプログラムを作る）
+// public class Main {
+//   public static void main(String[] args) {
+//       calculate(25, 85);
+//   }
   
-  public static void calculate(int x, int y) {
-      int ans = x * y;
-      System.out.println(x + "*" + y + "=" + ans + " です");
-  }
-}
-（実行結果）
-25*85=2125 です
+//   public static void calculate(int x, int y) {
+//       int ans = x * y;
+//       System.out.println(x + "*" + y + "=" + ans + " です");
+//   }
+// }
+// （実行結果）
+// 25*85=2125 です
 
-4_2 実引数と仮引数
-・一般的に、メソッドに渡す値、受け取る変数をともに「引数」と言う
-・この引数は厳密に言い分けると、渡す値を「実引数」、受け取る変数を「仮引数」と言う
+// 4_2 実引数と仮引数
+// ・一般的に、メソッドに渡す値、受け取る変数をともに「引数」と言う
+// ・この引数は厳密に言い分けると、渡す値を「実引数」、受け取る変数を「仮引数」と言う
 
-5 メソッドの戻り値を使う方法
-・呼び出されたメソッドから、呼び出し元へ値を返すことを「値を戻す(返す)」と言い、この時戻される値を「戻り値」と呼ぶ
-（例：値を戻す構文での記述）
-public static 戻り値の型 メソッド名(引数リスト・・・) {
-  // メソッドが実行された時に動く処理
-  return 戻り値;
-}
-（例：calculateメソッドを戻り値を使って書き換える）
-public class Main {
-  public static void main(String[] args) {
-      int x = 25, y = 85;
-      int ans = calculate(x, y);
-      System.out.println(x + "*" + y + "=" + ans + " です");
-  }
+// 5 メソッドの戻り値を使う方法
+// ・呼び出されたメソッドから、呼び出し元へ値を返すことを「値を戻す(返す)」と言い、この時戻される値を「戻り値」と呼ぶ
+// （例：値を戻す構文での記述）
+// public static 戻り値の型 メソッド名(引数リスト・・・) {
+//   // メソッドが実行された時に動く処理
+//   return 戻り値;
+// }
+// （例：calculateメソッドを戻り値を使って書き換える）
+// public class Main {
+//   public static void main(String[] args) {
+//       int x = 25, y = 85;
+//       int ans = calculate(x, y);
+//       System.out.println(x + "*" + y + "=" + ans + " です");
+//   }
   
-  public static int calculate(int x, int y) {
-      return x * y;
-  }
-}
-（実行結果）
-25*85=2125 です
+//   public static int calculate(int x, int y) {
+//       return x * y;
+//   }
+// }
+// （実行結果）
+// 25*85=2125 です
 
-6 メソッドのオーバーロードの利用
-・Javaで同じ名前のメソッドを定義することを「オーバーロード(多重継承)」と言う
-・オーバーロードは、以下のように仮引数の型が異なっていたり、仮引数の個数が異なっている場合に利用可能
-（例：calculateメソッドを戻り値を使って書き換える）
-public class Main {
-  public static void main(String[] args) {
-      System.out.println(calculate(25, 85));
-      System.out.println(calculate(25.0, 85.0));
-      System.out.println(calculate(25, 85, 17));
-  }
+// 6 メソッドのオーバーロードの利用
+// ・Javaで同じ名前のメソッドを定義することを「オーバーロード(多重継承)」と言う
+// ・オーバーロードは、以下のように仮引数の型が異なっていたり、仮引数の個数が異なっている場合に利用可能
+// （例：calculateメソッドを戻り値を使って書き換える）
+// public class Main {
+//   public static void main(String[] args) {
+//       System.out.println(calculate(25, 85));
+//       System.out.println(calculate(25.0, 85.0));
+//       System.out.println(calculate(25, 85, 17));
+//   }
   
-  public static int calculate(int x, int y) {
-      return x * y;
-  }
+//   public static int calculate(int x, int y) {
+//       return x * y;
+//   }
 
-  public static double calculate(double x, double y) {
-      return x * y;
-  }
+//   public static double calculate(double x, double y) {
+//       return x * y;
+//   }
 
-  public static int calculate(int x, int y, int z) {
-      return x * y * z;
-  }
-}
-（実行結果）
-2125
-2125.0
-36125
-※似た言葉に「オーバーライド」がありますが、全く異なるものなので混同しないように注意
+//   public static int calculate(int x, int y, int z) {
+//       return x * y * z;
+//   }
+// }
+// （実行結果）
+// 2125
+// 2125.0
+// 36125
+// ※似た言葉に「オーバーライド」がありますが、全く異なるものなので混同しないように注意
 
-7 まとめ
-プログラムを組む上で、メソッドを使わないことはほとんどなく、必須の知識と言える。
-メソッドはコードを読みやすくしたり、修正や変更が簡単に済むなどメリットも豊富。メソッドを覚えて使いこなすことが大事。
+// 7 まとめ
+// プログラムを組む上で、メソッドを使わないことはほとんどなく、必須の知識と言える。
+// メソッドはコードを読みやすくしたり、修正や変更が簡単に済むなどメリットも豊富。メソッドを覚えて使いこなすことが大事。
+
+参照：ITを分かりやすく解説
+【Java入門】メソッドの使い方
+
+1 メソッドとは
+・メソッドとは、クラス内に書かれた処理（いくつかの処理をまとめたもの）のこと
+・メソッドには、処理を実行するための条件である「引数」（パラメータ）を渡すことができます。
+・そして、処理の実行結果を「戻り値」として返却します。
+・※複数の引数を指定する場合は「total(int a, int b）」のようにカンマ区切りで指定します。また引数を指定しない場合は「total()」のように記述。
+

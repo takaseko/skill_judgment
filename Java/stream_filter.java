1beb505f7b94aa3e81a4438e5eed2b265a0e4576
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-07-25 11:40:41 +0900

    java_stream_anymatch_basic_1_1

--- Java/stream_filter.java
+++ Java/stream_filter.java
@@ -1,88 +1,134 @@
-// 参照：AMG Solution
-// 【Java Stream API 超入門】filterメソッドを使って、条件でコレクション要素をフィルタリングする方法
+// // 参照：AMG Solution
+// // 【Java Stream API 超入門】filterメソッドを使って、条件でコレクション要素をフィルタリングする方法
 
-1 Stream APIとは？
-・Streamとは、Listなどのコレクションに対する操作をサポートするクラス
-・オブジェクトのListに対して、条件による絞り込みや合計値や平均値などの集計処理を行うメソッドが用意
-・for文の処理に比べて、処理がわかりやすいようにコードを記述できるのが特徴
+// 1 Stream APIとは？
+// ・Streamとは、Listなどのコレクションに対する操作をサポートするクラス
+// ・オブジェクトのListに対して、条件による絞り込みや合計値や平均値などの集計処理を行うメソッドが用意
+// ・for文の処理に比べて、処理がわかりやすいようにコードを記述できるのが特徴
 
-2 使い方
-基本的な流れは次のとおりです。
-・コレクションからStreamを取得
-・Streamに対して、絞り込みや要素の変換などの中間処理を行う
-・変換したStreamに対して結果の取得など行う終端処理を行う
+// 2 使い方
+// 基本的な流れは次のとおりです。
+// ・コレクションからStreamを取得
+// ・Streamに対して、絞り込みや要素の変換などの中間処理を行う
+// ・変換したStreamに対して結果の取得など行う終端処理を行う
 
-3 Streamの取得
-（例：「Tokyo」、「Chiba」、「Saitama」、「Kanagawa」、「Gunma」、「Tochigi」、「Ibaraki」、「Yamanashi」の8つの要素を持つ文字列のList）
-List<String> list = Arrays.asList(
-  "Tokyo",
-  "Chiba",
-  "Saitama",
-  "Kanagawa",
-  "Gunma",
-  "Tochigi",
-  "Ibaraki",
-  "Yamanashi"
-);
-Stream<String> stream = list.stream();
+// 3 Streamの取得
+// （例：「Tokyo」、「Chiba」、「Saitama」、「Kanagawa」、「Gunma」、「Tochigi」、「Ibaraki」、「Yamanashi」の8つの要素を持つ文字列のList）
+// List<String> list = Arrays.asList(
+//   "Tokyo",
+//   "Chiba",
+//   "Saitama",
+//   "Kanagawa",
+//   "Gunma",
+//   "Tochigi",
+//   "Ibaraki",
+//   "Yamanashi"
+// );
+// Stream<String> stream = list.stream();
 
-4 中間処理（絞り込みや要素の変換などの中間処理）
-（例：絞り込みを行うStream.filter()メソッド）
-・filterメソッドは、条件でフィルタリングした新しいStreamを返却
-（例：文字列の長さが６文字以上という条件でフィルタリングを行う）
-Stream<String> stream2 = stream.filter(str -> str.length() >= 6);
-・ここではラムダ式というものを使っています。->の左に要素を受け取る引数を置き、右に条件式を記述
-・filterメソッドは、条件式を満たす要素のみの新しいStreamを返却します。これにより、文字数が６文字以上の要素だけからなるstream2が取得できます。
+// 4 中間処理（絞り込みや要素の変換などの中間処理）
+// （例：絞り込みを行うStream.filter()メソッド）
+// ・filterメソッドは、条件でフィルタリングした新しいStreamを返却
+// （例：文字列の長さが６文字以上という条件でフィルタリングを行う）
+// Stream<String> stream2 = stream.filter(str -> str.length() >= 6);
+// ・ここではラムダ式というものを使っています。->の左に要素を受け取る引数を置き、右に条件式を記述
+// ・filterメソッドは、条件式を満たす要素のみの新しいStreamを返却します。これにより、文字数が６文字以上の要素だけからなるstream2が取得できます。
 
-5 終端処理（結果の取得や表示など）
-stream2.foreEach(str -> System.out.println(str));
-・Stream.foreEach()は、ストリームの要素全てに対して１つずつ処理を実施するメソッド
-・Stream.foreEach()は、ストリームの要素全てに対して１つずつ処理を実施するメソッドです。
-・filterメソッドと同じで、ラムダ式によって->の左に要素を受け取る引数、右に処理の内容を記述します。
+// 5 終端処理（結果の取得や表示など）
+// stream2.foreEach(str -> System.out.println(str));
+// ・Stream.foreEach()は、ストリームの要素全てに対して１つずつ処理を実施するメソッド
+// ・Stream.foreEach()は、ストリームの要素全てに対して１つずつ処理を実施するメソッドです。
+// ・filterメソッドと同じで、ラムダ式によって->の左に要素を受け取る引数、右に処理の内容を記述します。
 
-6 for文との比較
-6_1 （例：拡張for文での記述）
-List<String> list = Arrays.asList(
-  "Tokyo",
-  "Chiba",
-  "Saitama",
-  "Kanagawa",
-  "Gunma",
-  "Tochigi",
-  "Ibaraki",
-  "Yamanashi"
-);
+// 6 for文との比較
+// 6_1 （例：拡張for文での記述）
+// List<String> list = Arrays.asList(
+//   "Tokyo",
+//   "Chiba",
+//   "Saitama",
+//   "Kanagawa",
+//   "Gunma",
+//   "Tochigi",
+//   "Ibaraki",
+//   "Yamanashi"
+// );
  
-for (String str : list) { // Listのすべての要素に対して処理を実施
-	if (str.length() >= 6) { // 文字列長が6文字以上という条件を満たした場合のみ表示を行う
-		System.out.println(str); 
-	}
-}
-（実行結果）
-Saitama
-Kanagawa
-Tochigi
-Ibaraki
-Yamanashi
+// for (String str : list) { // Listのすべての要素に対して処理を実施
+// 	if (str.length() >= 6) { // 文字列長が6文字以上という条件を満たした場合のみ表示を行う
+// 		System.out.println(str); 
+// 	}
+// }
+// （実行結果）
+// Saitama
+// Kanagawa
+// Tochigi
+// Ibaraki
+// Yamanashi
 
-6_2 （例：Stream APIでの記述）
-List<String> list = Arrays.asList(
-  "Tokyo",
-  "Chiba",
-  "Saitama",
-  "Kanagawa",
-  "Gunma",
-  "Tochigi",
-  "Ibaraki",
-  "Yamanashi"
-);
-Stream stream = list.stream(); // Streamの取得
-Stream stream2 = stream.filter(str -> str.length() >= 6); // 文字列長が6文字以上のものだけを集める中間処理
-stream2.foreEach(str -> System.out.println(str)); // 集めた要素をすべて表示する終端処理
-（実行結果）
-Saitama
-Kanagawa
-Tochigi
-Ibaraki
-Yamanashi
-for文が、すべての要素を１つずつ取り出し、条件を満たすか判定して満たすなら表示という処理となっているのに対し、Streamを用いた処理は条件を満たすものだけを集めて、集めたものをすべて表示するという処理になっています。
+// 6_2 （例：Stream APIでの記述）
+// List<String> list = Arrays.asList(
+//   "Tokyo",
+//   "Chiba",
+//   "Saitama",
+//   "Kanagawa",
+//   "Gunma",
+//   "Tochigi",
+//   "Ibaraki",
+//   "Yamanashi"
+// );
+// Stream stream = list.stream(); // Streamの取得
+// Stream stream2 = stream.filter(str -> str.length() >= 6); // 文字列長が6文字以上のものだけを集める中間処理
+// stream2.foreEach(str -> System.out.println(str)); // 集めた要素をすべて表示する終端処理
+// （実行結果）
+// Saitama
+// Kanagawa
+// Tochigi
+// Ibaraki
+// Yamanashi
+// for文が、すべての要素を１つずつ取り出し、条件を満たすか判定して満たすなら表示という処理となっているのに対し、Streamを用いた処理は条件を満たすものだけを集めて、集めたものをすべて表示するという処理になっています。
+
+参照：IT Sakura
+Java Stream APIでリストを操作する(stream)
+
+1 Stream APIとは
+・リストなどのコレクション、配列、ファイル等に使用します。
+・例：リスト等を指定してストリームを生成し、そのストリームに対して処理します。
+→Streamを生成→中間操作→終端操作
+・並列処理を行うこともできます。
+・並列で処理を行うため値が変わらない変数のみアクセス可能です。
+→外部のローカル変数に対してアクセスできるのは再代入不可のfinal変数のみです。
+→for文では外部のローカル変数にアクセスできます。
+
+1_1 コレクションでストリームを使用する
+コレクションではCollectionインターフェースのstreamメソッドを使用します。
+ArrayListなどのコレクションのクラスは、Collectionインターフェースを実装しているのでstreamメソッドを使用できます。
+
+2 要素の条件が１つでも一致するか(anyMatch)
+List<Integer> num1 = new ArrayList<>(Arrays.asList(5,8,9));
+  boolean a = num1.stream()
+    .anyMatch(b -> b > 10); //10より大きい要素はあるか
+  System.out.println(a); //false
+  boolean c = num1.stream()
+    .anyMatch(d -> d > 8); //8より大きい要素はあるか
+  System.out.println(c); //true
+（実行結果）要素の条件が1つでも一致した場合はtrueをそうでない場合はfalseを返します。
+
+3 要素の条件が全て一致するか(allMatch)
+List<Integer> num1 = new ArrayList<>(Arrays.asList(5,8,9));   
+boolean a = num1.stream()
+  .allMatch(b -> b > 7); //すべての要素は7より大きいか
+System.out.println(a); //false
+boolean c = num1.stream()
+  .allMatch(d -> d > 3); //すべての要素は3より大きいか
+System.out.println(c); //true
+（実行結果）すべての要素の条件が一致した場合はtrueをそうでない場合はfalseを返します。
+
+4 要素の条件が一致するものは１つもないか(noneMatch)
+List<Integer> num1 = new ArrayList<>(Arrays.asList(5,8,9));   
+boolean a = num1.stream()
+  .noneMatch(b -> b > 7); //7より大きい要素はひとつもないか
+System.out.println(a); //false   
+boolean c = num1.stream()
+  .noneMatch(d -> d > 10); //10より大きい要素はひとつもないか
+System.out.println(c); //true
+（実行結果）noneMatchメソッドは一致が1つもない場合にtrueを返します。

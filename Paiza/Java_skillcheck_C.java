5c8059014ff0d53b84a285768f956b8dd9477669
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-09-10 05:59:20 +0900

    skillcheck-086

--- Paiza/Java_skillcheck_C.java
+++ Paiza/Java_skillcheck_C.java
@@ -73,3 +73,30 @@ public class C016 {
 //        System.out.println(String.join("", result));
     }
 }
+
+// 1 C086
+import java.util.*;
+​
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine(); //変換する前の値
+      
+      List<String> vowels = Arrays.asList("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"); // 母音のリスト
+      List<String> strList = new ArrayList<>(Arrays.asList(s.split(""))); // 与えられた文字列をArrayListに変換
+      Iterator<String> ite = strList.iterator(); // iteratorに変換
+      
+      while (ite.hasNext()) {
+          if (vowels.contains(ite.next())) {
+              ite.remove();
+          }
+      }
+      // ↑strList.removeIf(vowels::contains); に書き換え可能
+      
+      // 母音を覗いた文字列の出力
+      for (String str : strList) {
+          System.out.print(str);
+      }
+    }
+}
+

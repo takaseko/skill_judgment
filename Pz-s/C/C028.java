afa1586e08a9f77bca98335f8051efd1908d02fa
髙瀬康平 <yasaiseikatu0306@gmail.com>
2021-10-09 11:24:46 +0900

    C-add

--- Pz-s/C/C028.java
+++ Pz-s/C/C028.java
@@ -12,7 +12,7 @@ public class C028 {
     sc.nextLine(); // 改行
     // System.out.println(); // 受付入力確認済
 
-    // 与えられた文字列のリスト
+    // 与えられた文字列のリスト（正解数、解答）
     List<List<String>> list = new ArrayList<>(); 
     for (int i = 0; i < N; i++) {  // 問題数分繰り返す
       String str = sc.nextLine();
@@ -20,27 +20,54 @@ public class C028 {
       list.add(Arrays.asList(array)); //配列をリストへ変換したものをadd（追加）する
     }
     // System.out.println(list); // リスト格納確認済
-    
-    String perfect; // 完全一致の文字列を初期化
-    int s = 0; // 理系科目を初期化
-    int l = 0; // 文系科目を初期化
-    int passTest = 0; // 試験通過した人数を初期化
+  
+    int totalPoints = 0; // 合計点数
+    // int differ = 0; // 間違い時の点数
     
     // 与えられた入力全件に対して以下の処理をする 例)[[apple, aple]]
     for (List<String> subList : list) {
-      // if文を入れ子にして記述
-      // 解答が完全一致の場合
-      // 全ての科目を足した結果を変数allSubに代入
-      if (subList.equals(subList)) {
-            passTest += 2; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
-      }
-        
-      // 解答が一文字違いの場合
-      if (subList) {
-        passTest += 1; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
-  }
+      // 解答が完全一致の場合 +2点
+      if (subList.get(0).equals(subList.get(1))) {
+            totalPoints += 2; // 合計点数を変数(totalPoints)へ足す
+      } else if (subList.get(0).length() != subList.get(1).length()) { // 解答の長さが違う場合
+        totalPoints += 0; // 足される点数はなし（0点）
+      } else if (subList.get(0).length() == subList.get(1).length()) { // 解答が1文字違いの場合 +1点
+        totalPoints += 1; // 1文字違い時の点数を変数(totalPoints)へ足す
+      // それ以外（解答が2文字以上違う）場合
+      } else { 
+        totalPoints += 0; // 足される点数はなし（0点）
+      } 
+      System.out.println(totalPoints);
     }
-    // for文内で条件処理して、最終的に試験通過した人数をfor文の外で出力
-    System.out.println(passTest);
+    // for文内で条件処理して、最終的な合計点数をfor文の外で出力
+    System.out.println(totalPoints);
   }
 }
+
+
+
+// for (List<String> subList2 : list) {
+//   // 解答の長さが違う場合
+//   if (subList2.get(0).length() != subList2.get(1).length()) { 
+//     differ += 0; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
+//   } 
+  
+//   if (subList2.get(0).length() == subList2.get(1).length()) { // 解答が一文字違いの場合
+//     differ -= 1; // 以上で間違い時の点数を変数(differ)へ足す
+//   } else { // それ以外（解答が2文字以上違う）場合
+//     differ += 0;
+//   } 
+// }
+
+
+// // if文を記述（入れ子のパターン）
+// // 解答が完全一致の場合
+// if (subList.get(0).equals(subList.get(1))) {
+//       totalPoints += 2; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
+// } else if (subList.get(0).length() != subList.get(1).length()) { // 解答の長さが違うの場合
+//   totalPoints += 0; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
+// } else if (subList.get(0).length() == subList.get(1).length()) { // 解答が一文字違いの場合
+//   totalPoints += 1; // 全科目合計が350点以上、かつ数学,理科の合計が160点以上で試験通過した人を変数(passTest)へ足す
+// } else { // それ以外（解答が2文字以上違う）場合
+//   totalPoints += 0;
+// }      
---
+++ Pz-s/C/C093.java
@@ -0,0 +1,87 @@
+package C;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+import java.util.Scanner;
+
+public class C093 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String[] array = sc.nextLine().split(" ");
+    System.out.println();
+
+    int bob = getResult(array[0].split(""));
+    int alice = getResult(array[1].split(""));
+
+    if (bob < alice) {
+        System.out.println("Alice");
+    } else if (alice < bob){
+        System.out.println("Bob");
+    } else {
+        System.out.println("Draw");
+    }
+    System.out.println();
+  }
+
+  public static int getResult(String[] array) {
+      int sum;
+      if (1 < array.length) {
+          sum = Integer.parseInt(array[0]) + Integer.parseInt(array[1]);
+      } else {
+          sum = Integer.parseInt(array[0]);
+      }
+      System.out.println();
+
+      String sumStr = String.valueOf(sum);
+      if (sumStr.length() == 1) {
+          return Integer.parseInt(sumStr);
+      } else {
+          return Integer.parseInt(sumStr.substring(1));
+      }
+  }
+}
+
+
+
+
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         //まず以下で入力受付
+//         int X = sc.nextInt(); // 点数（男性）
+//         int Y = sc.nextInt(); // 点数（女性）
+//         sc.nextLine(); // 改行
+//         // System.out.println(); // 受付確認
+        
+//         List<List<Integer>> Xlist = new ArrayList<>(); // 入力を一時的に保持するList
+//             // 子カードを枚数だけ処理を繰り返す。
+//             for (int i = 0; i < 1; i++) {
+//                 List<Integer> X = new ArrayList<>(); 
+//                 X.add(Integer.parseInt(array[0])); // 子カード1つ目の番号
+//                 X.add(Integer.parseInt(array[1])); // 子カード2つ目の番号
+//                 Xlist.add(X);
+//                 System.out.println(); // 子カードの格納確認OK
+//             }
+        
+//         //     // 結果の出力
+//         //     // 入力を一時的に保持するList全件に対して処理を繰り返す。
+//         //     for (List<Integer> integerList : list) {
+//           //         if (a > integerList.get(0)) { // 1つ目の番号で親カードが大きい場合
+//         //             System.out.println("High");
+//         //         }
+        
+//         //         if (a < integerList.get(0)) { // 1つ目の番号で親カードが小さい場合
+//           //             System.out.println("Low");
+//           //         }
+          
+//         //         if (a == integerList.get(0)) { // 1つ目の番号で親カードと同じで、2つ目の番号で親カードが大きい場合と小さい場合
+//           //             if (b < integerList.get(1)) {
+//             //                 System.out.println("High");
+//             //             } else {
+//         //                 System.out.println("Low");
+//         //             }
+//         //         }
+//         //     }
+//       }
+// }
